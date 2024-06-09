b0d4a7c99b643cbe861c7e522286be1f58e90a07
TezcanEmre <tezcan_emre@icloud.com>
2024-06-10 01:58:41 +0300

    menü için table view oluşturdum daha sonra devam edeceğim

---
+++ CalculaWiz/Views/MenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuTableViewCell.swift
+//  CalculaWiz
+//
+//  Created by Perseus on 10.06.2024.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
